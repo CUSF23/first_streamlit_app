497cbb73b4fce1c9b568811181b38289645c64ff
CUSF23 <142667823+CUSF23@users.noreply.github.com>
2023-08-19 17:44:34 +0530

    Update streamlit_app.py
    Added few contents

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@ import streamlit
 
 streamlit.title("My Parents Healthy Diner")
 
+
+streamlit.header("BreakFast Menu")
+streamlit.text("Iddli & Sambhar")
+steamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothiee")
+streamlit.text('HArd-Boilded Free-Range Egg')

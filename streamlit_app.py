7e0da7d75d2228f45119763b5df84abbdf8f46f0
CUSF23 <142667823+CUSF23@users.noreply.github.com>
2023-08-20 01:30:58 +0530

    Update streamlit_app.py
    added insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,8 @@ my_data_row = my_cur.fetchall()
 streamlit.dataframe(my_data_row)
 fruit_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', fruit_choice)
+
+my_cur.execute("INSERT into fruit_load_list values ('from streamlit')")
+
+
+

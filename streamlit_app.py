f836612633eefb70765a1db8dc280013c26fd86e
CUSF23 <142667823+CUSF23@users.noreply.github.com>
2023-08-19 23:50:26 +0530

    Update streamlit_app.py
    Beautified the json

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 
 streamlit.header('Fruityvice Fruit Advice')
 streamlit.text(fruityvice_response.json())
+
+### BEautify the json
+fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)

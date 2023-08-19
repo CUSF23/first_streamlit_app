ddd2d4cb6c990cf8cc1903c76c7a108b30032d6f
CUSF23 <142667823+CUSF23@users.noreply.github.com>
2023-08-19 17:47:08 +0530

    Update streamlit_app.py
    fixed a typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.title("My Parents Healthy Diner")
 
 streamlit.header('BreakFast Menu')
 streamlit.text("Iddli & Sambhar")
-steamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothiee")
 streamlit.text('HArd-Boilded Free-Range Egg')

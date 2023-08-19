c19a09edcfbe10ae11daaad467a929b9e52b78bd
CUSF23 <142667823+CUSF23@users.noreply.github.com>
2023-08-19 23:46:44 +0530

    Update streamlit_app.py
    fixed typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-import request
+import requests
 
 
 streamlit.title("My Parents Healthy Diner")
@@ -25,5 +25,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display on the page
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
